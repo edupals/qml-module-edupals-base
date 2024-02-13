fc3d858ba1adb217e9ddd3d1b61dfb63b875df36
Lt-Henry <quiqueiii@gmail.com>
2024-02-13 16:16:53 +0100

    Initial release. Working plugin allows fetching some current user info (name, groups, uid,...)

---
+++ src/plugin.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2024 Edupals project
+ *
+ * Author:
+ *  Enrique Medina Gremaldos <quiqueiii@gmail.com>
+ *
+ * Source:
+ *  https://github.com/edupals/qml-module-edupals-base
+ *
+ * This file is a part of qml-module-edupals-base.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#include "plugin.h"
+
+#include <user.hpp>
+#include <group.hpp>
+
+#include <QQmlEngine>
+#include <QAbstractItemModel>
+#include <QMimeData>
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+User::User()
+{
+    edupals::system::User me = edupals::system::User::me();
+
+    m_name = QString::fromStdString(me.name);
+    m_uid = me.uid;
+    m_gid = me.gid;
+
+    m_group = QString::fromStdString(me.group().name);
+
+    for (auto group:me.groups()) {
+        m_groups<<QString::fromStdString(group.name);
+    }
+
+}
+
+User::~User()
+{
+}
+
+BasePlugin::BasePlugin(QObject* parent) : QQmlExtensionPlugin(parent)
+{
+}
+
+void BasePlugin::registerTypes(const char* uri)
+{
+    qmlRegisterType<User> (uri, 1, 0, "User");
+    qmlRegisterAnonymousType<QMimeData>(uri, 1);
+    
+}
---
+++ src/plugin.h
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2024 Edupals project
+ *
+ * Author:
+ *  Enrique Medina Gremaldos <quiqueiii@gmail.com>
+ *
+ * Source:
+ *  https://github.com/edupals/qml-module-edupals-base
+ *
+ * This file is a part of qml-module-edupals-base.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#ifndef QML_EDUPALS_BASE_PLUGIN
+#define QML_EDUPALS_BASE_PLUGIN
+
+#include <system.hpp>
+#include <user.hpp>
+
+#include <QQmlExtensionPlugin>
+#include <QObject>
+#include <QVariant>
+#include <QString>
+#include <QList>
+#include <QThread>
+#include <QVariant>
+#include <QJSValue>
+
+class User: public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QString name MEMBER m_name)
+    Q_PROPERTY(uint32_t uid MEMBER m_uid)
+    Q_PROPERTY(uint32_t gid MEMBER m_gid)
+
+    Q_PROPERTY(QString group MEMBER m_group)
+    Q_PROPERTY(QStringList groups MEMBER m_groups)
+
+public:
+
+    User();
+    ~User();
+
+protected:
+
+    QString m_name;
+    uint32_t m_uid;
+    uint32_t m_gid;
+    QString m_group;
+    QStringList m_groups;
+
+};
+
+class BasePlugin : public QQmlExtensionPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA (IID "Edupals.Base")
+
+public:
+    explicit BasePlugin(QObject *parent = nullptr);
+    void registerTypes(const char *uri) override;
+};
+
+#endif
